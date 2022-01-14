77e8af780a195ddd9b4b660be2199160d19ce151
David Koloski <djkoloski@gmail.com>
2022-01-14 07:55:59 -0500

    Reinterpret cast options to avoid code duplication

--- src/impls/core/option.rs
+++ src/impls/core/option.rs
@@ -1,75 +1,60 @@
 use crate::{Intern, InternSerializeRegistry};
 use rkyv::{
-    ser::Serializer,
-    with::{ArchiveWith, DeserializeWith, SerializeWith},
     option::ArchivedOption,
-    Fallible,
-    out_field
+    ser::Serializer,
+    with::{ArchiveWith, DeserializeWith, SerializeWith, With},
+    Archive, Deserialize, Fallible, Serialize,
 };
-use core::{hint::unreachable_unchecked, ptr};
-
-#[allow(dead_code)]
-#[repr(u8)]
-enum ArchivedOptionTag {
-    None,
-    Some,
-}
-
-#[repr(C)]
-struct ArchivedOptionVariantNone(ArchivedOptionTag);
-
-#[repr(C)]
-struct ArchivedOptionVariantSome<T>(ArchivedOptionTag, T);
 
 impl<T> ArchiveWith<Option<T>> for Intern
-    where Intern: ArchiveWith<T>
+where
+    Intern: ArchiveWith<T>,
 {
     type Archived = ArchivedOption<<Intern as ArchiveWith<T>>::Archived>;
     type Resolver = Option<<Intern as ArchiveWith<T>>::Resolver>;
 
-    unsafe fn resolve_with(field: &Option<T>, pos: usize, resolver: Self::Resolver, out: *mut Self::Archived) {
-        match resolver {
-            None => {
-                let out = out.cast::<ArchivedOptionVariantNone>();
-                ptr::addr_of_mut!((*out).0).write(ArchivedOptionTag::None);
-            },
-            Some(resolver) => {
-                let out = out.cast::<ArchivedOptionVariantSome<<Intern as ArchiveWith<T>>::Archived>>();
-                ptr::addr_of_mut!((*out).0).write(ArchivedOptionTag::Some);
-
-                let value = if let Some(value) = field.as_ref() {
-                    value
-                } else {
-                    unreachable_unchecked();
-                };
-
-                let (fp, fo) = out_field!(out.1);
-                <Intern as ArchiveWith<T>>::resolve_with(value, pos + fp, resolver, fo);
-            }
-        }
+    unsafe fn resolve_with(
+        field: &Option<T>,
+        pos: usize,
+        resolver: Self::Resolver,
+        out: *mut Self::Archived,
+    ) {
+        // SAFETY: With is repr(transparent) so Option<With<T, W>> has the same layout as Option<T>
+        let field = &*(field as *const Option<T> as *const Option<With<T, Intern>>);
+        field.resolve(pos, resolver, out.cast());
     }
 }
 
-impl<T, S: InternSerializeRegistry<T> + Serializer + ?Sized> SerializeWith<Option<T>, S> for Intern
-    where Intern: ArchiveWith<T>,
-          Intern: SerializeWith<T, S>
+impl<T, S> SerializeWith<Option<T>, S> for Intern
+where
+    S: InternSerializeRegistry<T> + Serializer + ?Sized,
+    Intern: ArchiveWith<T>,
+    Intern: SerializeWith<T, S>,
 {
     fn serialize_with(field: &Option<T>, serializer: &mut S) -> Result<Self::Resolver, S::Error> {
-        match field {
-            None => Ok(None),
-            Some(field) => Ok(Some(<Intern as SerializeWith<T, S>>::serialize_with(field, serializer)?)),
+        unsafe {
+            // SAFETY: With is repr(transparent) so Option<With<T, W>> has the same layout as
+            // Option<T>
+            let field = &*(field as *const Option<T> as *const Option<With<T, Intern>>);
+            field.serialize(serializer)
         }
     }
 }
 
-impl<T, D: Fallible + ?Sized> DeserializeWith<ArchivedOption<<Intern as ArchiveWith<T>>::Archived>, Option<T>, D> for Intern
-    where Intern: ArchiveWith<T>,
-          Intern: DeserializeWith<<Intern as ArchiveWith<T>>::Archived, T, D>
+impl<T, D> DeserializeWith<ArchivedOption<<Intern as ArchiveWith<T>>::Archived>, Option<T>, D>
+    for Intern
+where
+    D: Fallible + ?Sized,
+    Intern: ArchiveWith<T>,
+    Intern: DeserializeWith<<Intern as ArchiveWith<T>>::Archived, T, D>,
 {
-    fn deserialize_with(field: &ArchivedOption<<Intern as ArchiveWith<T>>::Archived>, deserializer: &mut D) -> Result<Option<T>, D::Error> {
-        match field {
-            ArchivedOption::None => Ok(None),
-            ArchivedOption::Some(field) => Ok(Some(<Intern as DeserializeWith<<Intern as ArchiveWith<T>>::Archived, T, D>>::deserialize_with(field, deserializer)?)),
-        }
+    fn deserialize_with(
+        field: &ArchivedOption<<Intern as ArchiveWith<T>>::Archived>,
+        deserializer: &mut D,
+    ) -> Result<Option<T>, D::Error> {
+        Ok(
+            Deserialize::<Option<With<T, Intern>>, D>::deserialize(field, deserializer)?
+                .map(|x| x.into_inner()),
+        )
     }
 }
